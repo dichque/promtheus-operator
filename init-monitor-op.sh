933bf4f33033417c9be079ab84b500a55549106f
Jagadish Nagarajaiah <jagadish.nagarajaiah@gmail.com>
2020-03-03 19:45:19 +0530

    Pointed images to internal repo

--- init-monitor-op.sh
+++ init-monitor-op.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ## Prometheus operator deployment to strimzi ns
-oc login -u system:admin
+# oc login -u system:admin
 oc apply -f prometheus-crd.yml -n strimzi
+oc create secret generic alertmanager-alertmanager --from-file=alertmanager.yaml -n strimzi
 oc process -f prometheus-operator.yml -p NAMESPACE=strimzi -p KAFKA_SERVICE_TEAM=aims | oc apply -f -
-
+# oc process -f prometheus.yml -p NAMESPACE=kafka-1 | oc apply -f -
